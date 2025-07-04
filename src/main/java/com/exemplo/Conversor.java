132146cb2004746723a332f389da991889800b1b
lfcgama <lfcgama@yahoo.com.br>
2025-07-03 22:55:40 -0400

    Cenário 1: Tudo certo

--- src/main/java/com/exemplo/Conversor.java
+++ src/main/java/com/exemplo/Conversor.java
@@ -3,7 +3,7 @@ package com.exemplo;
 public class Conversor {
     public static double fahrenheitToCelsius(double f) {
         return (f - 32) * 5 / 9;
-    
+    }
     public static double celsiusToFahrenheit(double c) {
         return c * 9 / 5 + 32;
     }
